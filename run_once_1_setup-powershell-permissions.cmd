c8f56fb533f31326298f3242e7e921295364e1af
Lyze <11274700+lyze237@users.noreply.github.com>
2024-04-18 14:07:57 +0200

    Fix cmd scripts

--- run_once_1_setup-powershell-permissions.cmd
+++ run_once_1_setup-powershell-permissions.cmd
@@ -1,3 +1,6 @@
 {{ if eq .chezmoi.os "windows" -}}
 powershell.exe -Command "Start-Process -FilePath PowerShell.exe -Verb Runas -ArgumentList \"Set-ExecutionPolicy Bypass\""
+{{ else -}}
+#!/usr/bin/env bash
+echo hi
 {{ end -}}
--- run_once_2_install-powershell-packages.cmd
+++ run_once_2_install-powershell-packages.cmd
@@ -1,4 +1,7 @@
 {{ if eq .chezmoi.os "windows" -}}
  "Install-Module -Scope CurrentUser -Force PSReadLine"
 powershell.exe -Command "Install-Module -Scope CurrentUser -Force Get-ChildItemColor" -AllowClobber
+{{ else -}}
+#!/usr/bin/env bash
+echo hi
 {{ end -}}
